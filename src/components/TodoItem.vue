d872ad0ff33ed97ba7c40f49f6d6b87a65fea6a2
rorast <rorast.power.game@gmail.com>
2019-04-16 00:39:52 +0800

    feat: Finish Vue-cli 3 TodoList

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <li @click="handleDelete">{{ content }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  // 接收父組件的參數
+  props: ['content','index'],
+  methods: {
+    handleDelete () {
+      // 發射訊息給父組件，並將 index 索引值帶過去
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div>
+            <input v-model="work"/>
+            <button @click="handleSubmit">提交</button>
+        </div>
+        <ul>
+          <todo-item v-for="(item,index) of list" 
+            :key="index"
+            :content="item"
+            :index="index"
+            @delete="handleDelete"
+            >
+            {{item}}
+            </todo-item>
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+// 以前用new Vue的時候是對象，在這裡是對象中的組件
+export default {
+    components: {
+        TodoItem
+    },
+    // data: function() {  // ES5的語法
+    data () {              // ES6的語法
+        return {
+            work: '',
+            list: []
+        }
+    },
+    methods: {
+        handleSubmit () {
+            this.list.push(this.work)
+            this.work = ''
+        },
+        handleDelete (index) {
+            this.list.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
